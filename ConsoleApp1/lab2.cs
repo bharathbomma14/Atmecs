62a9d9eeeb7f21008da4cdb1eb6d444b68db4189
bharathbomma14 <124856760+bharathbomma14@users.noreply.github.com>
2024-02-05 17:48:07 +0530

    Add project files.

---
+++ ConsoleApp1/lab2.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace consoleApp1
+{
+    internal class Stock
+    {
+        string? name;
+        double price;
+        int shares;
+        static void Main(string[] args)
+        {
+            Stock ibm = new Stock();
+            ibm.Initialize("Ibm", 1200, 100);
+            Console.WriteLine(ibm.Value());
+        }
+        void Initialize(string name, double price, int shares)
+        {
+            this.name = name;
+            this.price = price;
+            this.shares = shares;
+        }
+        void Buy(int shares)
+        {
+            this.shares += shares;
+        }
+        void Sell(int shares)
+        {
+            this.shares -= shares;
+        }
+        void SetPrice(double Price)
+        {
+            this.price += Price;
+        }
+        double Value()
+        {
+            return price * shares;
+        }
+    }
+
+}
+
